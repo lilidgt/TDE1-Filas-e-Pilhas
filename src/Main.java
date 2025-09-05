f87be5b4c59833e60a977f044e97f4b75f73b2ad
andressa-barros <andressa.barros4194@gmail.com>
2025-09-05 16:31:58 -0300

    Teste de fila com lista estruturada

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,24 @@ public class Main {
     //só para testar
     
     public static void main(String[] args) {
-            Pilha pilha = new Pilha();
+        //TESTE PILHA
+//            Pilha pilha = new Pilha();
+//
+//            pilha.inserir(10);
+//            pilha.inserir(20);
+//            pilha.inserir(30);
+//            pilha.imprimir();
+//            pilha.remover();
+//            pilha.imprimir();
 
-            pilha.inserir(10);
-            pilha.inserir(20);
-            pilha.inserir(30);
-
-            // Mostra o topo (se tiver peek)
-            pilha.imprimir();
-            pilha.remover();
-            // Ou percorre toda a pilha
-            pilha.imprimir();
+        //TESTE FILA
+//        Fila fila = new Fila();
+//        fila.inserir(10);
+//        fila.inserir(20);
+//        fila.imprimir();
+//        fila.remover();
+//        fila.imprimir();
+//        fila.remover();
+//        fila.imprimir();
     }
 }
